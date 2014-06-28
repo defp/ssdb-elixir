6116b5fa51ef79c207d48fd73af2ce64799c47f2
lidashuang <lidashuang@pinidea.com>
2014-06-29 00:41:49 +0800

    parse response by cmd type

--- lib/ssdb/server.ex
+++ lib/ssdb/server.ex
@@ -36,8 +36,8 @@ defmodule SSDB.Server do
   end
 
   defp handle_response(data, state) do
-    new_queue = data 
-                |> parse_binary 
+    new_queue = data
+                |> parse_binary
                 |> reply(state.queue)
     %State{state | queue: new_queue}
   end
@@ -73,15 +73,6 @@ defmodule SSDB.Server do
     end
   end
 
-  defp parse_binary(""), do: []
-  defp parse_binary("\n"), do: []
-
-  defp parse_binary(binary) do
-    {size, "\n" <> rest} = Integer.parse(binary)
-    <<chunk :: [binary, size(size)], "\n", rest :: binary>> = rest
-    [chunk|parse_binary(rest)]
-  end
-
   defp create_request(args) do
     bin = Enum.map(Enum.map(args, fn(arg) -> to_binary(arg) end),
       fn(arg) -> "#{byte_size(arg)}\n#{arg}\n" end)
@@ -93,15 +84,94 @@ defmodule SSDB.Server do
   defp to_binary(x) when is_atom(x), do: Atom.to_string(x)
   defp to_binary(x) when is_list(x), do: List.to_string(x)
 
+  defp parse_binary(""), do: []
+  defp parse_binary("\n"), do: []
+
+  defp parse_binary(binary) do
+    {size, "\n" <> rest} = Integer.parse(binary)
+    <<chunk :: [binary, size(size)], "\n", rest :: binary>> = rest
+    [chunk|parse_binary(rest)]
+  end
+
   defp ssdb_response(response, command) do
     [status | values] = response
     case status do
-      "ok" -> {:ok, values}
-      "not_found" -> {:not_found, values}
-      "error" -> {:error, values}
-      "fail" -> {:fail, values}
-      "client_error" -> {:client_error, values}
+      "ok" -> {:ok, get_reply(command, values)}
+      "not_found" -> {:not_found}
+      "error" -> {:error, List.first(values)}
+      "fail" -> {:fail, List.first(values)}
+      "client_error" -> {:client_error, List.first(values)}
     end
   end
 
+  defp get_reply("zavg", values) do
+    List.first(values) |> String.to_float
+  end
+
+  @bool_reply ["exists", "hexists", "zexists"]
+  for cmd <- @bool_reply do
+    defp get_reply(unquote(cmd), values) do
+      List.first(values) == "1"
+    end
+  end
+
+  @multi_reply ["keys", "zkeys", "hkeys", "hlist", "zlist", "qslice"]
+  for cmd <- @multi_reply do
+    defp get_reply(unquote(cmd), values) do
+      values
+    end
+  end
+
+  @multi_bool_reply ["multi_exists", "multi_hexists", "multi_zexists"]
+  for cmd <- @multi_bool_reply do
+    defp get_reply(unquote(cmd), values) do
+      list_to_bool_map(values)
+    end
+  end
+
+  @kv_reply ["scan","rscan","zscan","zrscan","zrange","zrrange","hscan","hrscan",
+    "hgetall","multi_hsize","multi_zsize","multi_get","multi_hget","multi_zget"]
+  for cmd <- @kv_reply do
+    defp get_reply(unquote(cmd), values) do
+      list_to_map(values)
+    end
+  end
+
+  @single_reply ["get","substr","getset","hget","qget","qfront", "qback",
+    "qpop","qpop_front","qpop_back"]
+  for cmd <- @single_reply do
+    defp get_reply(unquote(cmd), values) do
+      List.first(values)
+    end
+  end
+
+  @false_or_value_reply ["getbit", "setbit", "countbit", "strlen", "set", "setx",
+    "setnx", "zset", "hset", "qpush", "qpush_front", "qpush_back", "del", "zdel",
+    "hdel", "hsize", "zsize", "qsize", "hclear", "zclear", "qclear", "multi_set",
+    "multi_del", "multi_hset", "multi_hdel", "multi_zset", "multi_zdel", "incr",
+    "decr", "zincr", "zdecr", "hincr", "hdecr", "zget", "zrank", "zrrank", "zcount",
+    "zsum", "zremrangebyrank", "zremrangebyscore"]
+  for cmd <-@false_or_value_reply do
+    defp get_reply(unquote(cmd), values) do
+      value = List.first(values)
+      case value do
+        "0" -> false
+        _  -> value
+      end
+    end
+  end
+
+  defp list_to_map([]), do: %{}
+  defp list_to_map(list) do
+    [key, value | rest] = list 
+    map = Map.put(%{}, key, value)
+    Map.merge(map, list_to_map(rest))
+  end
+
+  defp list_to_bool_map([]), do: %{}
+  defp list_to_bool_map(list) do
+    [key, value | rest] = list 
+    map = Map.put(%{}, key, value == "1")
+    Map.merge(map, list_to_map(rest))
+  end
 end
--- test/ssdb_elixir_test.exs
+++ test/ssdb_elixir_test.exs
@@ -1,7 +1,14 @@
 defmodule SSDBTest do
   use ExUnit.Case
 
-  test "the truth" do
+  setup do 
+    {:ok, pid} = SSDB.start
+    {:ok, pid: pid}
+  end
+
+  test "set", %{pid: pid} do
+    {:ok, v} = SSDB.get pid, "a"
+    IO.puts v
     assert 1 + 1 == 2
   end
 end

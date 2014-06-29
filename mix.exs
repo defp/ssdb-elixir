2d5573d97f925eaa5e4c7c77375fcb3a39231cc4
lidashuang <lidashuang@pinidea.com>
2014-06-29 18:43:16 +0800

    update mix file

--- mix.exs
+++ mix.exs
@@ -5,26 +5,27 @@ defmodule SSDB.Mixfile do
     [app: :ssdb_elixir,
      version: "0.1.1",
      elixir: "~> 0.14.2-dev",
-     deps: deps]
+     deps: deps,
+     description: description,
+     source_url: "https://github.com/lidashuang/ssdb_elixir",
+     package: package ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp description do
+    "SSDB client for Elixir"
+  end
+
   defp deps do
     []
+  end
+
+  defp package do
+    [contributors: ["lidashuang"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/lidashuang/ssdb_elixir"}]
   end
 end

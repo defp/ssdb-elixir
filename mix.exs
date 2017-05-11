d6bd698775710ad8fc3e58b8f3349f9ee651dba1
lidashuang <ldshuang@gmail.com>
2017-05-11 11:02:47 +0800

    fix: console warning

--- mix.exs
+++ mix.exs
@@ -2,13 +2,15 @@ defmodule SSDB.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ssdb,
-     version: "0.3.0",
-     elixir: "~> 1.0.0",
-     deps: deps,
-     description: description,
-     source_url: "https://github.com/lidashuang/ssdb-elixir",
-     package: package ]
+    [
+      app: :ssdb,
+      version: "0.3.0",
+      elixir: ">= 1.0.0",
+      deps: deps(),
+      description: description(),
+      source_url: "https://github.com/lidashuang/ssdb-elixir",
+      package: package()
+    ]
   end
 
   def application do
@@ -24,8 +26,10 @@ defmodule SSDB.Mixfile do
   end
 
   defp package do
-    [contributors: ["lidashuang"],
-     licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/lidashuang/ssdb-elixir"}]
+    [
+      contributors: ["lidashuang"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/lidashuang/ssdb-elixir"}
+    ]
   end
 end

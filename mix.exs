5f6d53f3310cca9b347f293f4ba8428a7e4a3ba0
lidashuang <ldshuang@gmail.com>
2017-05-11 11:17:57 +0800

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -4,11 +4,13 @@ defmodule SSDB.Mixfile do
   def project do
     [
       app: :ssdb,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: ">= 1.0.0",
       deps: deps(),
       description: description(),
       source_url: "https://github.com/lidashuang/ssdb-elixir",
+      homepage_url: "https://github.com/lidashuang/ssdb-elixir",
+      docs: [main: "SSDB", extras: ["README.md"]],
       package: package()
     ]
   end
@@ -22,12 +24,13 @@ defmodule SSDB.Mixfile do
   end
 
   defp deps do
-    []
+    [{:ex_doc, "~> 0.14", only: :dev, runtime: false}]
   end
 
   defp package do
     [
       contributors: ["lidashuang"],
+      maintainers: ["lidashuang"],
       licenses: ["Apache 2.0"],
       links: %{"Github" => "https://github.com/lidashuang/ssdb-elixir"}
     ]
